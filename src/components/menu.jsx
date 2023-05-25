0fa824855642e79731b14c78c4f679def68f3b35
Hitchcock <cemekaoji@gmail.com>
2023-05-25 07:09:50 +0100

    added new routes

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -41,9 +41,9 @@ const Navbar = () => {
                             <ul>
                                 <li><a className="sd-link" href="/">Home<FaHome/></a> </li>
                                 <li><a className="sd-link" href="/about">About<FaUser/></a></li>
-                                <li><a className="sd-link" href="about">Projects<FaTools/></a></li>
-                                <li><a className="sd-link" href="about">Contact<FaMailBulk/></a></li>
-                                <li><a className="sd-link" href="about"  id="download"> Download Resume<FaCloudDownloadAlt color="black"/></a></li>
+                                <li><a className="sd-link" href="/">Projects<FaTools/></a></li>
+                                <li><a className="sd-link" href="/">Contact<FaMailBulk/></a></li>
+                                <li><a className="sd-link" href="/"  id="download"> Download Resume<FaCloudDownloadAlt color="black"/></a></li>
                                 <li><a className="sd-link" href="https://bmc.link/hitchcock" id="coffee" target="_blank" rel="noreferrer"> Buy me a coffee<CiCoffeeCup color="black"/></a></li>
                             </ul>
                         </div>
