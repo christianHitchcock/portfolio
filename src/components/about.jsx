8d6662fbeda4fd39573cf1d7c8bf9b6a2e913bd3
Hitchcock <cemekaoji@gmail.com>
2023-05-25 14:42:15 +0100

    added history section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -20,29 +20,36 @@ const About = () => {
         </div>
         <div>
             <h1>Hello, I'm Christian.</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>I'm extremely passionate about technology and design. I specialize in frontend programming, creating elegant user interfaces and websites. I also offer photography, video editing, and graphic design services.</p>
         </div>
         <div>
         <h2 onClick={() => showContainer(!hideContainer)}>History
                 {hideContainer ? <FaArrowCircleDown className="faicon"/>   :  <FaArrowCircleRight  className="faicon"/>}</h2>
        
                 {hideContainer && <div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                     Excepteur sint occaecat cupidatat non proident, 
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                      ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                      Excepteur sint occaecat cupidatat non proident, 
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>following unsuccessful attempts to pursue computer science in university. I made the decision to give it another try and pursue my passion for technology.
+                       I graduated with a good degree from the department of geography and environmental management. 
+                        However, I continued to be interested in computer-related issues. During Covid I worked for a Wi-Fi company as an network operation intern. 
+                       learned about the internet of things. fell in love with technology. I was able to restart my tech career because of my interest in IoT and smart devices.
+                       I completed some iot classes on YouTube and reached the section where I had to figure out how to write with Python and the Raspberry Pi.
+                       At that point, I made the decision to enroll in a boot camp and learn how to program.
+                        without any prior programming experience, without any instructions or mentors. 
+                        There was no one I could turn to for any useful information, so I made the biggest choice of my life.
+                         I relocated to a new place and began my tech career.
+                       I gained programming and website development skills using php, HTML, and CSS.
+                       A few websites were developed before I finally learnt Python. developing more projects with Kivy and Tinkita.
+                       Upon completing this boot camp. I volunteered to teach a few new students whilst learning a lot. I was still unsure about what to do next. 
+                       I started looking for guidance from mentors and senior engineers when I was still locked in tutorial hell.  
+                       I made the decision to study JavaScript after receiving instruction from a mentor. Obtaining a contract to create a website on the side
+                        Without any prior understanding of WordPress, I built and developed this website.
+                       I used my earnings from this employment to pay for a full stack development course by Angela Yu on Udemy. This course really opened my eyes.
+                        This course improved my knowledge of JavaScript, node.js, react.js, databases, and other topics.
+                       i started building more projects with MERN stack .  then  Focused my attention on frontend development and designs.
+                        Built several projects from frontend mentor and still building.
+                        Got a few friends from twitter and we started building projects together and communicating through WhatsApp and discord. 
+                        This have given me better experiences and I have gotten better at programming. Studying and practicing everyday
+                         I love creating and building stuffs.
+                       Thanks for reading 😊</p>
                     </div>}
         <p></p>
         </div>
